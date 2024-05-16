df0f35a482dd4fdf169708533b5d1d4c9dbe820c
nicolasdepalma <depalmanicolasm@gmail.com>
2024-05-16 15:33:07 -0300

    challange17

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -149,9 +149,9 @@ suite('Unit Tests', function () {
     });
     // #17
     test('#typeOf, #notTypeOf', function () {
-      assert.typeOfypeOf(myCar, 'object');
+      assert.typeOf(myCar, 'object');
       assert.typeOf(myCar.model, 'string');
-      assert.notTypeOfypeOf(airlinePlane.wings, 'string');
+      assert.notTypeOf(airlinePlane.wings, 'string');
       assert.typeOf(airlinePlane.engines, 'array');
       assert.typeOf(myCar.wheels, 'number');
     });
