3e20fcf3f0503206b4a4b4aa4d35893c1616b71c
nicolasdepalma <depalmanicolasm@gmail.com>
2024-05-16 13:09:19 -0300

    challange2

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,9 +10,9 @@ suite('Unit Tests', function () {
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.isUndefined(null, 'null is not undefined');
-      assert.isDefined(undefined, 'undefined IS undefined');
-      assert.isUndefined('hello', 'A string is not undefined');
+      assert.isDefined(null, 'null is not undefined');
+      assert.isUndefined(undefined, 'undefined IS undefined');
+      assert.isDefined('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
