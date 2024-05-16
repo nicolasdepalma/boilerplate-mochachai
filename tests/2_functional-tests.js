3b5765b56659b225ff9efa8734814170677a03c4
nicolasdepalma <depalmanicolasm@gmail.com>
2024-05-16 16:31:13 -0300

    challenge4

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -50,17 +50,17 @@ suite('Functional Tests', function () {
         });
     });
     // #4
-    test('Send {surname: "da Verrazzano"}', function (done) {
+    test('Send {surname:"da Verrazzano"}', function (done) {
       chai
         .request(server)
         .keepOpen()
         .put('/travellers')
-        .send({surname:'Colomboda Verrazzano'})
+        .send({surname:'da Verrazzano'})
         .end(function (err, res) {
-      assert.equal(res.status, 200);
+          assert.equal(res.status, 200);
           assert.equal(res.type,'application/json');
-          assert.equal(res.body.name,"Giovanni");
-          assert.equal(res.body.surname,"da Verrazzano");
+          assert.equal(res.body.name,'Giovanni');
+          assert.equal(res.body.surname,'da Verrazzano');
 
       done();
     });
