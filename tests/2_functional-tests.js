dccc556eeba995b17448ef099f98c0e3332ce577
nicolasdepalma <depalmanicolasm@gmail.com>
2024-05-16 16:29:06 -0300

    challenge4

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -51,7 +51,12 @@ suite('Functional Tests', function () {
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      .end(function (err, res) {
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
+        .send({surname:'Colomboda Verrazzano'})
+        .end(function (err, res) {
       assert.equal(res.status, 200);
           assert.equal(res.type,'application/json');
           assert.equal(res.body.name,"Giovanni");
