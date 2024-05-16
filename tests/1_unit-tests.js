8221aef0f9e8b448e117be10256aae5a13497e61
nicolasdepalma <depalmanicolasm@gmail.com>
2024-05-16 15:37:27 -0300

    challenge18

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -160,7 +160,7 @@ suite('Unit Tests', function () {
       assert.notInstanceOf(myCar, Plane);
       assert.instanceOf(airlinePlane, Plane);
       assert.instanceOf(airlinePlane, Object);
-      assert.instanceOfc(myCar.wheels, String);
+      assert.notInstanceOfc(myCar.wheels, String);
     });
   });
 
